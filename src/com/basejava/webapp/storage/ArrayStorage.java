b9e930e695185c6314fbc72e8e89ada027b298af
Evgenii Vlasov <evjvlasov@gmail.com>
2019-11-24 21:25:16 +0300

    Update ArrayStorage

---
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -0,0 +1,78 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private int size = 0;
+    private Resume[] storage = new Resume[10000];
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
+    }
+
+    public void update(Resume r){
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
+        }
+    }
+
+    public void save(Resume r) {
+        if (r.getUuid() != null) {
+            storage[size] = r;
+            size = size + 1;
+        }
+    }
+
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+            return null;
+        }
+        return storage[index];
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
+
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        Resume[] result = new Resume[size];
+        System.arraycopy(storage, 0, result, 0, size);
+        return result;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size ; i++) {
+           if (storage[i].getUuid().equals(uuid)) {
+               return i;
+           }
+        }
+        return -1;
+    }
+}
